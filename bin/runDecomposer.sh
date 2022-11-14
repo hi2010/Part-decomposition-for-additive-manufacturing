f67445645c20f08076a758835b7c61804b553df0
Louis Hätinger <st142399@stud.uni-stuttgart.de>
2022-11-14 10:37:21 +0100

    added file

---
+++ bin/runDecomposer.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo "run this script from bin folder if cmake install was used"
+SCRIPT_PATH=`realpath "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
+echo in script
+echo $SCRIPT_DIR;
+
+export PATH=$SCRIPT_DIR/lib:$PATH
+export LD_LIBRARY_PATH=$SCRIPT_DIR/lib:$LD_LIBRARY_PATH
+echo appended path, this path is:
+echo $PATH
+echo appended ld_library_path, this ld... is:
+echo $LD_LIBRARY_PATH
+$SCRIPT_DIR/bin/decomposer $@
